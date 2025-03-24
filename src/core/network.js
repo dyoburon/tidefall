f903295d8d2fddb73115bac875a2fe21a5993cf9
thenamesdyl <burtndylan@gmail.com>
2025-03-23 22:59:23 -0700

    Update network.js

--- src/core/network.js
+++ src/core/network.js
@@ -19,8 +19,8 @@ const DEFAULT_MESSAGE_LIMIT = 50;
 let cannonHitCallback = null;
 
 // Network configuration
-const SERVER_URL = 'http://localhost:5001';
-//const SERVER_URL = 'https://boat-game-python.onrender.com';
+//const SERVER_URL = 'http://localhost:5001';
+const SERVER_URL = 'https://boat-game-python.onrender.com';
 
 // Network state
 export let socket;
